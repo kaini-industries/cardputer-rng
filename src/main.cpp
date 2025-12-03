621448f3edc04f6569e04391d1146b23e11e1b21
Eric Anderson <beatsforthemind@gmail.com>
2025-12-02 20:07:43 -0600

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,65 +2,102 @@
 #include <Crypto.h>
 #include <SHA256.h>
 
+SHA256 hashMachine = SHA256();
+
 m5::imu_data_t imuData;
 
-String entropy = "";
-
-union FloatBytes {
+struct FloatBytes {
   float f_val;
   byte b_array[sizeof(float)]; // sizeof(float) is typically 4 bytes // ??
 };
 
+/*
+union FloatBytes {
+  float f_val;
+  byte b_array[sizeof(float)]; // sizeof(float) is typically 4 bytes // ??
+};
+*/
+
+String entropy = "";
+String floatTest = ""; // ??
+
+// String allByteValuesStr = "";
+// FloatBytes floatData; // TEST VAR // NEEDS RESET ??
+
 void setup() {
   auto cfg = M5.config();
   M5Cardputer.begin(cfg, true);
-  // randomSeed(analogRead(0));
+
+  // INITS
+  M5.Imu.update();
+  imuData = M5.Imu.getImuData();
+
+  // randomSeed(analogRead(0)); // hm...
   M5Cardputer.Display.setFont(&fonts::FreeMonoBold9pt7b);
   M5Cardputer.Display.setCursor(0, 0);
 
-  FloatBytes floatData; // ??
+  // RESETS // WHICH ORDER FOR THESE // DO THESE NEED TO BE HERE
+  String allByteValuesStr = "";
+  // hashMachine.reset();
+  // hashMachine.clear();
 }
 
 void loop() {
+  // SHA256 object
+  SHA256 hashMachine = SHA256();
+
   M5Cardputer.update();
 
-  //
-  int number = random(0, 9);
-  M5Cardputer.Display.setCursor(0, 0);
-  M5Cardputer.Display.print(String(number));
-  //
+  std::__cxx11::string allByteValuesStr = "";
+
+  // WORKS
+  // int number = random(0, 9);
+  // M5Cardputer.Display.setCursor(0, 0);
+  // M5Cardputer.Display.print(String(number)+" PULSE");
 
   M5.Imu.update();
   imuData = M5.Imu.getImuData();
 
-  SHA256 pulesHash = SHA256();
+  String pulseHashStr = "";
+  // String floatTest = ""; // ??
+  std::string floatTest = ""; // ??
 
-  String aHash = "";
-
-  String floatTest = "";
-  FloatBytes floatData; // ?? NEEDS RESET?
-  byte floatDataArr[] = {}; // ?? NEEDS RESET?
+  FloatBytes floatData; // TEST VAR // NEEDS RESET ??
+  byte floatDataArr[] = {}; // NEEDS BETTER RESET ??
 
   FloatBytes accelX;
+  accelX.f_val = imuData.accel.x;
   // uint8_t accelXArr[] = {};
   byte accelXArr[] = {};
+
   FloatBytes gyroX;
+  gyroX.f_val = imuData.gyro.x;
   // uint8_t gyroXArr[] = {};
   byte gyroXArr[] = {};
+
   FloatBytes accelY;
+  accelY.f_val = imuData.gyro.x;
   // uint8_t accelYArr[] = {};
   byte accelYArr[] = {};
+
   FloatBytes gyroY;
+  gyroY.f_val = imuData.gyro.y;
   // uint8_t gyroYArr[] = {};
   byte gyroYArr[] = {};
+
   FloatBytes accelZ;
+  accelZ.f_val = imuData.accel.y;
   // uint8_t accelZArr[] = {};
   byte accelZArr[] = {};
+
   FloatBytes gyroZ;
+  gyroZ.f_val = imuData.gyro.z;
   // uint8_t gyroZArr[] = {};
   byte gyroZArr[] = {};
 
-  floatData.f_val = imuData.accel.x; // TEST
+  floatData.f_val = imuData.gyro.x; // TEST
+  // M5Cardputer.Display.setCursor(0, 20);
+  // M5Cardputer.Display.print(allByteValuesStr);
 
   accelX.f_val = imuData.accel.x;
   gyroX.f_val = imuData.gyro.x;
@@ -69,58 +106,98 @@ void loop() {
   accelZ.f_val = imuData.accel.z;
   gyroZ.f_val = imuData.gyro.z;
 
-  M5Cardputer.Display.setCursor(0, 0); // ??
-
-  // byte myArray[] = {0x10, 0x2A, 0xFF, 0x05}; // Array size determined by initializer list
+  // byte myArray[] = {0x10, 0x2A, 0xFF, 0x05}; // TEST ARRAY OF BYTES
   for (int i = 0; i < sizeof(floatData.b_array); i++) {
+    // M5Cardputer.Display.setCursor(0, 20);
+    // M5Cardputer.Display.print(String(floatData.b_array[i]));
+    // delay(1000);
+
+    // STRING TO BYTE DOES NOT WORK cool
     String byteValue = "";
 
     floatTest += "0x";
     byteValue += "0x";
-    
+    allByteValuesStr += "0x";
+
     if (floatData.b_array[i] < 0x10) {
       floatTest += "0";
       byteValue += "0";
+      allByteValuesStr += "0";
     } 
-    // Serial.print(floatData.b_array[i], HEX); // ??
-    floatTest += String(floatData.b_array[i]);
+    // floatTest += String(floatData.b_array[i]);
     byteValue += String(floatData.b_array[i]);
+    // allByteValuesStr += String(floatData.b_array[i]);
+    // std::__cxx11::string cFloatStr = String(floatData.b_array[i]);
+    // allByteValuesStr = allByteValuesStr + String(floatData.b_array[i]);
 
-    // floatDataArr[i] = (byte) byteValue; // FIX THIS
-    floatDataArr[i] = (byte) floatData.b_array[i]; // FIX THIS OR CHECK FOR ZERO BYTES
+    // floatDataArr[i] = (byte) byteValue; // STRING TO BYTE DOES NOT WORK
+
+    floatDataArr[i] = (byte) floatData.b_array[i]; // FIX THIS OR CHECK FOR ZERO BYTES // ??
   }
 
-  M5Cardputer.Display.setCursor(0, 20);
-  // M5Cardputer.Display.print(String(floatTest));
-  // M5Cardputer.Display.print(floatDataArr); // NO
+  // 
+  // M5Cardputer.Display.setCursor(0, 20);
+  // M5Cardputer.Display.print(allByteValuesStr);
 
-  // uint8_t data[3] = {0x16, 0xF4, 0x00}; // Example: 5876 = 0x16F4, fill as needed
-  pulesHash.reset();
-  pulesHash.update(floatDataArr, sizeof(floatDataArr));
-  uint8_t hash[32];
-  pulesHash.finalize(hash, sizeof(hash));
-  for (int i = 0; i < sizeof(hash); i++) {
-    char buf[3];
-    sprintf(buf, "%02x", hash[i]);
-    aHash += buf;
+  // HASHing floatTest or somethimg
+  std::string testString = allByteValuesStr;
+  std::string myStdString = testString;
+  
+  // const char* cStyleString = myStdString.c_str(); 
+  // M5Cardputer.Display.setCursor(0, 20);
+  // M5Cardputer.Display.print(String(cStyleString));
+
+  //+ const char* cStyleString = myStdString.c_str(); 
+  // std::string myStdString = String(allByteValuesStr);
+  // const char* cStyleString = myStdString.c_str(); 
+
+  uint8_t hash[hashMachine.HASH_SIZE];
+  // hashMachine.update((const uint8_t*) cStyleString, strlen(cStyleString)); // Feed the data
+  hashMachine.finalize(hash, sizeof(hash)); // Finalize and get the hash
+  for (size_t i = 0; i < hashMachine.HASH_SIZE; i++) {
+    // M5Cardputer.Display.print(hash[i], HEX); // Print each byte in hexadecimal
+    // pulseHashStr += hash[i];
+    // entropy += hash[i];
   }
-  M5Cardputer.Display.setCursor(0, 20);
-  M5Cardputer.Display.print(String(aHash));
 
-  // RESETS
-  pulesHash.reset();
-  // floatDataArr[] = {};
+  //+ M5Cardputer.Display.setCursor(0, 20);
+  //+ M5Cardputer.Display.print(pulseHashStr);
 
   /*
+  hashMachine.update(floatDataArr, sizeof(floatDataArr));
+  uint8_t hash[32]; // THE HASH TO BE CREATED // SIZE ??
+  hashMachine.finalize(hash, sizeof(hash)); // hash or not
+
+  for (int i = 0; i < sizeof(hash); i++) {
+    char buf[3];
+    sprintf(buf, "%02x", hash[i]); // ??
+    // pulseHashStr += buf;
+
+    M5Cardputer.Display.setCursor(0, 20);
+    // M5Cardputer.Display.print(hash);
+    M5Cardputer.Display.print((hash[i]));
+  }
+  */
+
+  // entropy += String(hash); // NOT HERE
+
+  // RESETS // WHICH ORDER FOR THESE
+  hashMachine.reset();
+  hashMachine.clear();
+  // floatDataArr[] = {};
+
+  // int entropyLength = entropy.length();
+
+  // M5Cardputer.Display.setCursor(0, 20);
+  // M5Cardputer.Display.print(String(entropyLength));
+
+  // M5Cardputer.Display.setCursor(0, 40);
+  // M5Cardputer.Display.print(String(entropy));
+
+  /* // CHARGING DEMO
   bool isCharging = M5Cardputer.Power.isCharging();
   int batteryLevel = M5Cardputer.Power.getBatteryLevel();
   int batteryVoltage = M5Cardputer.Power.getBatteryVoltage();
-  */
-
-  /*
-  int number = random(0, 9);
-  M5Cardputer.Display.setCursor(0, 0);
-  M5Cardputer.Display.print(String(number));
   */
 
   /*
@@ -132,36 +209,5 @@ void loop() {
   M5Cardputer.Display.print(String(entropyLength));
   */
 
-  /*
-  M5Cardputer.Display.setCursor(120, 55);
-  M5Cardputer.Display.printf("%3d %%", batteryLevel);
-  M5Cardputer.Display.setCursor(120, 72);
-  M5Cardputer.Display.printf("%4d mV", batteryVoltage);
-  */
-
-  delay(1000);
-}
-
-int handleSingleImuRead(int read) {
-  int result;
-
-  result = read;
-
-  if (read < 0) {
-    result = read * -1;
-  }
-
-  return result;
-}
-
-int handleGroupedImuRead(int read) {
-  int result;
-
-  result = read;
-
-  if (read < 0) {
-    result = read * -1;
-  }
-
-  return result;
+  delay(1200);
 }
